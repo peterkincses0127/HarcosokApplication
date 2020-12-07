0894437991d5e1bcdb63dd2bdb2c9fb960d7d8dc
Péter Kincses <peter.kincsesj@gmail.com>
2020-12-07 15:45:18 +0100

    Add project files.

---
+++ HarcosokApplication/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace HarcosokApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.harcosNeveTextBox = new System.Windows.Forms.TextBox();
+            this.letrehozBtn = new System.Windows.Forms.Button();
+            this.hasznaloComboBox = new System.Windows.Forms.ComboBox();
+            this.leirasTextbox = new System.Windows.Forms.TextBox();
+            this.kepessegNeveTextBox = new System.Windows.Forms.TextBox();
+            this.kepessegHozzaad = new System.Windows.Forms.Button();
+            this.harcosokListBox = new System.Windows.Forms.ListBox();
+            this.kepessegekListBox = new System.Windows.Forms.ListBox();
+            this.kepessegLeirasTextBox = new System.Windows.Forms.TextBox();
+            this.torles = new System.Windows.Forms.Button();
+            this.modosit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // harcosNeveTextBox
+            // 
+            this.harcosNeveTextBox.Location = new System.Drawing.Point(65, 28);
+            this.harcosNeveTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.harcosNeveTextBox.Name = "harcosNeveTextBox";
+            this.harcosNeveTextBox.Size = new System.Drawing.Size(119, 20);
+            this.harcosNeveTextBox.TabIndex = 0;
+            // 
+            // letrehozBtn
+            // 
+            this.letrehozBtn.Location = new System.Drawing.Point(203, 26);
+            this.letrehozBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.letrehozBtn.Name = "letrehozBtn";
+            this.letrehozBtn.Size = new System.Drawing.Size(82, 19);
+            this.letrehozBtn.TabIndex = 1;
+            this.letrehozBtn.Text = "Létrehozás";
+            this.letrehozBtn.UseVisualStyleBackColor = true;
+            this.letrehozBtn.Click += new System.EventHandler(this.letrehozBtn_Click);
+            // 
+            // hasznaloComboBox
+            // 
+            this.hasznaloComboBox.FormattingEnabled = true;
+            this.hasznaloComboBox.Location = new System.Drawing.Point(65, 82);
+            this.hasznaloComboBox.Margin = new System.Windows.Forms.Padding(2);
+            this.hasznaloComboBox.Name = "hasznaloComboBox";
+            this.hasznaloComboBox.Size = new System.Drawing.Size(92, 21);
+            this.hasznaloComboBox.TabIndex = 2;
+            // 
+            // leirasTextbox
+            // 
+            this.leirasTextbox.Location = new System.Drawing.Point(184, 80);
+            this.leirasTextbox.Margin = new System.Windows.Forms.Padding(2);
+            this.leirasTextbox.Multiline = true;
+            this.leirasTextbox.Name = "leirasTextbox";
+            this.leirasTextbox.Size = new System.Drawing.Size(211, 98);
+            this.leirasTextbox.TabIndex = 3;
+            // 
+            // kepessegNeveTextBox
+            // 
+            this.kepessegNeveTextBox.Location = new System.Drawing.Point(65, 123);
+            this.kepessegNeveTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.kepessegNeveTextBox.Name = "kepessegNeveTextBox";
+            this.kepessegNeveTextBox.Size = new System.Drawing.Size(76, 20);
+            this.kepessegNeveTextBox.TabIndex = 4;
+            // 
+            // kepessegHozzaad
+            // 
+            this.kepessegHozzaad.Location = new System.Drawing.Point(65, 159);
+            this.kepessegHozzaad.Margin = new System.Windows.Forms.Padding(2);
+            this.kepessegHozzaad.Name = "kepessegHozzaad";
+            this.kepessegHozzaad.Size = new System.Drawing.Size(92, 19);
+            this.kepessegHozzaad.TabIndex = 5;
+            this.kepessegHozzaad.Text = "Hozzáad";
+            this.kepessegHozzaad.UseVisualStyleBackColor = true;
+            this.kepessegHozzaad.Click += new System.EventHandler(this.kepessegHozzaad_Click);
+            // 
+            // harcosokListBox
+            // 
+            this.harcosokListBox.FormattingEnabled = true;
+            this.harcosokListBox.Location = new System.Drawing.Point(65, 225);
+            this.harcosokListBox.Margin = new System.Windows.Forms.Padding(2);
+            this.harcosokListBox.Name = "harcosokListBox";
+            this.harcosokListBox.Size = new System.Drawing.Size(92, 108);
+            this.harcosokListBox.TabIndex = 6;
+            this.harcosokListBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.harcosokListBox_MouseDoubleClick);
+            // 
+            // kepessegekListBox
+            // 
+            this.kepessegekListBox.FormattingEnabled = true;
+            this.kepessegekListBox.Location = new System.Drawing.Point(184, 225);
+            this.kepessegekListBox.Margin = new System.Windows.Forms.Padding(2);
+            this.kepessegekListBox.Name = "kepessegekListBox";
+            this.kepessegekListBox.Size = new System.Drawing.Size(88, 108);
+            this.kepessegekListBox.TabIndex = 7;
+            this.kepessegekListBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.kepessegekListBox_MouseDoubleClick);
+            // 
+            // kepessegLeirasTextBox
+            // 
+            this.kepessegLeirasTextBox.Location = new System.Drawing.Point(307, 224);
+            this.kepessegLeirasTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.kepessegLeirasTextBox.Multiline = true;
+            this.kepessegLeirasTextBox.Name = "kepessegLeirasTextBox";
+            this.kepessegLeirasTextBox.Size = new System.Drawing.Size(166, 108);
+            this.kepessegLeirasTextBox.TabIndex = 8;
+            // 
+            // torles
+            // 
+            this.torles.Location = new System.Drawing.Point(184, 337);
+            this.torles.Margin = new System.Windows.Forms.Padding(2);
+            this.torles.Name = "torles";
+            this.torles.Size = new System.Drawing.Size(56, 19);
+            this.torles.TabIndex = 9;
+            this.torles.Text = "Törlés";
+            this.torles.UseVisualStyleBackColor = true;
+            this.torles.Click += new System.EventHandler(this.torles_Click);
+            // 
+            // modosit
+            // 
+            this.modosit.Location = new System.Drawing.Point(477, 225);
+            this.modosit.Margin = new System.Windows.Forms.Padding(2);
+            this.modosit.Name = "modosit";
+            this.modosit.Size = new System.Drawing.Size(83, 19);
+            this.modosit.TabIndex = 10;
+            this.modosit.Text = "Módosít";
+            this.modosit.UseVisualStyleBackColor = true;
+            this.modosit.Click += new System.EventHandler(this.modosit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Harcos létrehozása";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 30);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Név:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 58);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Képesség hozááadása";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 85);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Használó:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 127);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Név:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(182, 67);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Leírás:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(63, 209);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Harcosok:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(182, 209);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Képességei:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(316, 209);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(92, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Képesség leírása:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 403);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.modosit);
+            this.Controls.Add(this.torles);
+            this.Controls.Add(this.kepessegLeirasTextBox);
+            this.Controls.Add(this.kepessegekListBox);
+            this.Controls.Add(this.harcosokListBox);
+            this.Controls.Add(this.kepessegHozzaad);
+            this.Controls.Add(this.kepessegNeveTextBox);
+            this.Controls.Add(this.leirasTextbox);
+            this.Controls.Add(this.hasznaloComboBox);
+            this.Controls.Add(this.letrehozBtn);
+            this.Controls.Add(this.harcosNeveTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Harcos létrehozása";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox harcosNeveTextBox;
+        private System.Windows.Forms.Button letrehozBtn;
+        private System.Windows.Forms.ComboBox hasznaloComboBox;
+        private System.Windows.Forms.TextBox leirasTextbox;
+        private System.Windows.Forms.TextBox kepessegNeveTextBox;
+        private System.Windows.Forms.Button kepessegHozzaad;
+        private System.Windows.Forms.ListBox harcosokListBox;
+        private System.Windows.Forms.ListBox kepessegekListBox;
+        private System.Windows.Forms.TextBox kepessegLeirasTextBox;
+        private System.Windows.Forms.Button torles;
+        private System.Windows.Forms.Button modosit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ HarcosokApplication/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace HarcosokApplication
+{
+    public partial class Form1 : Form
+    {
+        MySqlCommand sql = null;
+        MySqlConnection conn = null;
+        public Form1()
+        {
+          
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder sb = new MySqlConnectionStringBuilder();
+            sb.Server = "localhost";
+            sb.UserID = "root";
+            sb.Password = "";
+            sb.Database = "cs_harcosok";
+            sb.CharacterSet = "utf8";
+
+             conn = new MySqlConnection(sb.ToString());
+           
+
+            try
+            {
+                conn.Open();
+                sql = conn.CreateCommand();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                Environment.Exit(0);
+            }
+            
+            hasznaloComboBox.Items.Clear();
+            sql.CommandText = "SELECT `nev` FROM `harcosok` ";
+            using (MySqlDataReader dr = sql.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    hasznaloComboBox.Items.Add(dr.GetString("nev"));
+                }
+            }
+
+            sql.CommandText = "SELECT `nev` FROM `harcosok`";
+
+
+            using (MySqlDataReader dr = sql.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    harcosokListBox.Items.Add(dr.GetString("nev"));
+                }
+            }
+
+
+        }
+        private void letrehoz(string nev) {
+       
+            sql.CommandText = "INSERT INTO `harcosok`(`id`, `nev`, `letrehozas`) VALUES (null,'" + nev + "', NOW())";
+            if (sql.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show("Sikeres!");
+
+
+            }
+            else
+            {
+                MessageBox.Show("Nem sikeres!");
+            }
+
+            hasznaloComboBox.Items.Clear();
+            sql.CommandText = "SELECT `nev` FROM `harcosok` ";
+            using (MySqlDataReader dr = sql.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    hasznaloComboBox.Items.Add(dr.GetString("nev"));
+                }
+            }
+
+            sql.CommandText = "SELECT `nev` FROM `harcosok`";
+
+            harcosokListBox.Items.Clear();
+
+            using (MySqlDataReader dr = sql.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    harcosokListBox.Items.Add(dr.GetString("nev"));
+                }
+            }
+        }
+        private void letrehozBtn_Click(object sender, EventArgs e)
+        {
+            letrehoz(harcosNeveTextBox.Text);
+
+        }
+        private void kepessegHozzaadas(string nev, string hasznaloneve)
+        {
+            sql.CommandText = "INSERT INTO `kepessegek`(`id`, `nev`, `leiras`, `harcos_id`) VALUES (DEFAULT,'" + nev + "','" + leirasTextbox.Text + "',(SELECT `id` FROM `harcosok` WHERE `nev` = '" + hasznaloneve + "'))";
+
+            if (hasznaloComboBox.SelectedIndex == -1 || kepessegNeveTextBox.Text.Length == 0 || leirasTextbox.Text.Length == 0)
+            {
+                MessageBox.Show("Rossz bemenet");
+            }
+            else
+            {
+                if (sql.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("Sikeres!");
+
+
+                }
+            }
+             
+
+        }
+        private void kepessegHozzaad_Click(object sender, EventArgs e)
+        {
+            if (hasznaloComboBox.SelectedIndex == -1 && kepessegNeveTextBox.Text.Length == 0 && leirasTextbox.Text.Length == 0)
+            {
+                MessageBox.Show("Rossz bemenet");
+            }
+            else
+            {
+                kepessegHozzaadas(kepessegNeveTextBox.Text, hasznaloComboBox.GetItemText(hasznaloComboBox.SelectedItem));
+            }
+
+        }
+
+        private void harcosokListBox_MouseDoubleClick_lekerdezes() {
+
+            kepessegekListBox.Items.Clear();
+            sql.CommandText = "select kepessegek.nev as knev from kepessegek join  harcosok on harcosok.id = kepessegek.harcos_id where (select harcosok.id from harcosok where harcosok.nev = '" + harcosokListBox.GetItemText(harcosokListBox.SelectedItem) + "')";
+
+            using (MySqlDataReader dr = sql.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    kepessegekListBox.Items.Add(dr.GetString("knev"));
+                }
+            }
+        }
+        private void harcosokListBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+            harcosokListBox_MouseDoubleClick_lekerdezes();
+          
+        }
+        private void kepessegekListBox_MouseDoubleClick_lekerdezes() {
+            sql.CommandText = "select leiras from kepessegek where nev = '" + kepessegekListBox.GetItemText(kepessegekListBox.SelectedItem) + "'";
+            using (MySqlDataReader dr = sql.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    kepessegLeirasTextBox.Text = dr.GetString("leiras");
+                }
+            }
+        }
+        private void kepessegekListBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            kepessegekListBox_MouseDoubleClick_lekerdezes();
+        }
+       
+        private void modosit_Click(object sender, EventArgs e)
+        {
+            string regiLeiras = kepessegekListBox.Text;
+            // sql.CommandText = "   UPDATE `kepessegek` SET `leiras`= '" + ujLeiras + "' WHERE  `nev` = '" + regiLeiras + "' ";
+            using (MySqlDataReader dr = sql.ExecuteReader())
+            {
+                dr.Read();
+           
+            }
+        }
+        private void torles_Click_lekerdezes(string torolKepesseg) {
+            sql.CommandText = "DELETE FROM `kepessegek` WHERE nev = '" + torolKepesseg + "' ";
+            if (kepessegekListBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Nincs kiválasztva elem!");
+            }
+            else
+            {
+
+                kepessegekListBox.Items.RemoveAt(kepessegekListBox.SelectedIndex);
+                kepessegLeirasTextBox.Text = "";
+                using (MySqlDataReader dr = sql.ExecuteReader())
+                {
+                    dr.Read();
+                }
+
+            }
+        }
+        private void torles_Click(object sender, EventArgs e)
+        {
+
+            torles_Click_lekerdezes(kepessegekListBox.GetItemText(kepessegekListBox.SelectedItem));
+
+        }
+    }
+}
